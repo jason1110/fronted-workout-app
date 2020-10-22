00ec8b198ba5a0657d25da73005c40defe73fc9a
jason1110 <Jason.easterly11@gmail.com>
2020-10-21 19:47:11 -0600

    override

--- user.js
+++ user.js
@@ -4,8 +4,8 @@ const search_term = searchParams.get("search_term")
 const baseURL = 'http://localhost:7000'
 let userPageURL =(`http://localhost:7000/users/${id})`)
 let workoutURL = `${baseURL}/workouts`
+const userURL = `${baseURL}/users`
 const searchForm = document.querySelector('#search-form')
-
 if(search_term) {
     workoutURL = `${workoutURL}?search_term=${search_term}`
 }
@@ -21,57 +21,32 @@ if(id){
         const h1 = document.createElement('h1')
         h1.textContent = `Welcome, ${user.username}`
         welcome.append(h1)
-        const programContainer = document.querySelector('#program-container')
-        user.workouts.forEach(workout => {
-        const li = document.createElement('li')
-        li.innerHTML = `<a href="workout.html?workout_id=${workout.id}">${workout.name}</a>`
-        programContainer.append(li)
-        })  
-    })
-            
         
-    
+        const programContainer = document.querySelector('#program-container')
+        user.workout.forEach(workouts => {
+        const li = document.createElement('li')
+        li.textContent = workouts.name
+        programContainer.append(li)
+        })
+    })
 
     fetch(workoutURL)
-        .then(response => response.json())
-        .then(workouts => {
-            const workoutCards = document.querySelector('.workout-card')
-            workouts.forEach(workout => {
-                const workoutName = document.createElement('h2')
-                const addWorkout = document.createElement('form')
-                addWorkout.method = 'POST'
-                addWorkout.action = 'http://localhost:7000/programs'
-                addWorkout.id = 'add-workout'
-                workoutName.innerHTML = `<a href="workout.html?workout_id=${workout.id}">${workout.name}</a>` 
-                const workoutIdInput = document.createElement('input')
-                const submitWorkout = document.createElement('input')
-                
-                const userIdInput = document.createElement('input')
-                
-                submitWorkout.value = "Add Workout"
-                submitWorkout.type = 'submit'
-
-                workoutIdInput.type = 'hidden'
-                workoutIdInput.value = workout.id
-
-                userIdInput.type = 'hidden'
-                userIdInput.value = id
-
-                workoutIdInput.name = 'workout_id'
-                userIdInput.name = 'user_id'
-                workoutCards.append(workoutName, addWorkout)
-                
-                addWorkout.append(workoutIdInput, userIdInput, submitWorkout)
-
-            })
+    .then(response => response.json())
+    .then(workouts => {
+        const workoutCards = document.querySelector('.workout-card')
+        workouts.forEach(workout => {
+        const h2 = document.createElement('h2')
+        h2.innerHTML = `<a href="workout.html?workout_id=${workout.id}">${workout.name}</a>`
+        workoutCards.append(h2)
         })
+    })
     
     
     fetch(workoutURL)
     .then(response => response.json())
     .then(workouts => {
+        const $workoutSelectType = document.getElementById('workout-search')
         workouts.forEach(workout => {
-            const $workoutSelectType = document.getElementById('workout-search')
             const $workoutDropDown = document.createElement('option')
             $workoutDropDown.innerText = workout.kind
             $workoutDropDown.value = `${workout.kind}`
